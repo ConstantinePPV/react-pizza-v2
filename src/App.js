76e0b28480a0a1b5e4e0290a213a9bce45a4c68f
ConstantinePPV <59139284+ConstantinePPV@users.noreply.github.com>
2023-03-02 22:59:59 +0400

    Lesson #7 is completed.

--- src/App.js
+++ src/App.js
@@ -1,46 +1,26 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 
 import Header from './components/Header'
-import Categories from './components/Categories'
-import Sort from './components/Sort'
-import PizzaBlock from './components/PizzaBlock'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import Cart from './pages/Cart'
 
 import './scss/app.scss'
 
 
 function App() {
-  const [items, setItems] = React.useState([])
-
-
-
-  React.useEffect(() => {
-    fetch('http://localhost:3001/pizzas')
-      .then((res) => res.json())
-      .then((arr) => setItems(arr))
-  }, [])
 
   return (
     <div className="wrapper">
       <Header/>
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories/>
-            <Sort/>
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {items.map(obj => (
-              // <PizzaBlock
-              //   title={obj.title}
-              //   price={obj.price}
-              //   imageUrl={obj.imageUrl}
-              //   sizes={obj.sizes}
-              //   types={obj.types}
-              // />
-              <PizzaBlock key={obj.id} {...obj}/>
-            ))}
-          </div>
+          <Routes>
+            <Route path={'/'} element={<Home/>}/>
+            <Route path={'/cart'} element={<Cart/>}/>
+            <Route path={'*'} element={<NotFound/>}/>
+          </Routes>
         </div>
       </div>
     </div>
