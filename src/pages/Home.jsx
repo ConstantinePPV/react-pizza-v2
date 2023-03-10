77afcc55acd69cdeaf8ef8849c72b66d895af277
ConstantinePPV <59139284+ConstantinePPV@users.noreply.github.com>
2023-03-10 23:40:50 +0400

    Lesson #9 is completed.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,31 +5,45 @@ import Skeleton from '../components/PizzaBlock/Skeleton'
 import PizzaBlock from '../components/PizzaBlock'
 
 const Home = () => {
-
   const [items, setItems] = React.useState([])
   const [isLoading, setIsLoading] = React.useState(true)
+  const [categoryId, setCategoryId] = React.useState(0)
+  const [sortType, setSortType] = React.useState({
+    name: 'популярности',
+    sortProperty: 'rating',
+  })
 
   React.useEffect(() => {
-    fetch('http://localhost:3001/pizzas')
+    setIsLoading(true)
+
+    const category = categoryId > 0 ? `category=${categoryId}` : ''
+    const sortBy = sortType.sortProperty.replace('-', '')
+    const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc'
+
+    fetch(`http://localhost:3001/pizzas?${category}&_sort=${sortBy}&_order=${order}`)
       .then((res) => res.json())
       .then((arr) => {
         setItems(arr)
         setIsLoading(false)
+        console.log(categoryId)
       })
     window.scrollTo(0, 0)
-  }, [])
+  }, [categoryId, sortType])
 
   return (
     <div className="container">
       <div className="content__top">
-        <Categories/>
-        <Sort/>
+        <Categories
+          value={categoryId}
+          onChangeCategory={(i) => setCategoryId(i)}
+        />
+        <Sort value={sortType} onChangeSort={(i) => setSortType(i)}/>
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
         {isLoading
           ? [...new Array(8)].map((_, index) => <Skeleton key={index}/>)
-          : items.map(obj => (<PizzaBlock key={obj.id} {...obj}/>))}
+          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
       </div>
     </div>
   )
