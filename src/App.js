7ca54741738e523e3daf063899ffaa8172887218
ConstantinePPV <59139284+ConstantinePPV@users.noreply.github.com>
2023-04-04 23:01:10 +0400

    Lesson #12 is completed.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './redux/slices/filterSlice'
+
 import Header from './components/Header'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
@@ -13,9 +16,26 @@ export const SearchContext = React.createContext('')
 
 function App() {
   const [searchValue, setSearchValue] = React.useState('')
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
 
   return (
     <div className="wrapper">
+      <button
+        aria-label="Increment value"
+        onClick={() => dispatch(increment())}
+      >
+        Increment
+      </button>
+      <span>{count}</span>
+      <button
+        aria-label="Decrement value"
+        onClick={() => dispatch(decrement())}
+      >
+        Decrement
+      </button>
+
+
       <SearchContext.Provider value={{searchValue, setSearchValue}}>
         <Header/>
         <div className="content">
