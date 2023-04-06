552330db0a3348eac337475c105f6ba72bbde8ce
ConstantinePPV <59139284+ConstantinePPV@users.noreply.github.com>
2023-04-06 22:08:38 +0400

    Lesson #14 is completed.

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 
+import debounce from 'lodash.debounce'
 import styles from './Search.module.scss'
 import { SearchContext } from '../../App'
 
 
 const Search = () => {
-  const {searchValue, setSearchValue} = React.useContext(SearchContext)
+  const [value, setValue] = React.useState('')
+  const {setSearchValue} = React.useContext(SearchContext)
+  const inputRef = React.useRef()
+
+  const onClickClear = () => {
+    setSearchValue('')
+    setValue('')
+    inputRef.current.focus()
+  }
+
+  const updateSearchValue = React.useCallback(
+    debounce((str) => {
+      setSearchValue(str)
+    }, 1000),
+    []
+  )
+
+  const onChangeInput = (event) => {
+    setValue(event.target.value)
+    updateSearchValue(event.target.value)
+  }
 
   return (
     <div className={styles.root}>
@@ -15,13 +36,14 @@ const Search = () => {
           d="M796 935 533 672q-30 26-69.959 40.5T378 727q-108.162 0-183.081-75Q120 577 120 471t75-181q75-75 181.5-75t181 75Q632 365 632 471.15 632 514 618 554q-14 40-42 75l264 262-44 44ZM377 667q81.25 0 138.125-57.5T572 471q0-81-56.875-138.5T377 275q-82.083 0-139.542 57.5Q180 390 180 471t57.458 138.5Q294.917 667 377 667Z"/>
       </svg>
       <input
-        value={searchValue}
-        onChange={(event) => setSearchValue(event.target.value)}
+        ref={inputRef}
+        value={value}
+        onChange={onChangeInput}
         className={styles.input}
         placeholder="Поиск пиццы..."
       />
-      {searchValue && (
-        <svg onClick={() => setSearchValue('')} className={styles.clearIcon}
+      {value && (
+        <svg onClick={onClickClear} className={styles.clearIcon}
              xmlns="http://www.w3.org/2000/svg" height="48"
              viewBox="0 96 960 960" width="48">
           <path
